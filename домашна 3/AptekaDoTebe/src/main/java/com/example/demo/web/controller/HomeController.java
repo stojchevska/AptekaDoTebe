ddbe2d200dee04d2917640640873bf3cdb86886c
stojchevska <stojcevska.ivana@gmail.com>
2020-12-19 00:20:10 +0100

    changes in controller

--- домашна 3/AptekaDoTebe/src/main/java/com/example/demo/web/controller/HomeController.java
+++ домашна 3/AptekaDoTebe/src/main/java/com/example/demo/web/controller/HomeController.java
@@ -1,18 +1,22 @@
 package com.example.demo.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class HomeController {
     @Controller
     @RequestMapping("/home")
     public class HomeController {
 
-        @GetMapping
-        public String getHomePage() {
-            return "home";
+        @GetMapping("/mk")
+        public String getHomePageMk() {
+            return "home_mk";
         }
 
-        @GetMapping
-        public String getHomePage() {
-            return "home";
+        @GetMapping("/en")
+        public String getHomePageEn() {
+            return "home_en";
         }
     }
 }
--- домашна 3/AptekaDoTebe/src/main/java/com/example/demo/web/controller/PharmacyController.java
+++ домашна 3/AptekaDoTebe/src/main/java/com/example/demo/web/controller/PharmacyController.java
@@ -1,33 +1,37 @@
 package com.example.demo.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class PharmacyController {
     @Controller
     @RequestMapping("/pharmacy")
     public class PharmacyController {
         @GetMapping("/all/mk")
         public String getAllPharmaciesMK() {
-            return "pharmacies";
+            return "pharmacy_mk";
         }
         @GetMapping("/map/mk")
         public String getMapMK(){
-            return "mapa";
+            return "map_mk";
         }
         @GetMapping("/help/mk")
         public String getHelpMK(){
-            return "help";
+            return "help_mk";
         }
 
         @GetMapping("/help/en")
         public String getHelpEN(){
-            return "helpEN";
+            return "help_en";
         }
         @GetMapping("/all/en")
         public String getAllPharmaciesEN(){
-            return "pharmaciesEN";
+            return "pharmacy_en";
         }
         @GetMapping("/map/en")
         public String getMapEN(){
-            return "mapaEN";
+            return "map_en";
         }
     }
 
