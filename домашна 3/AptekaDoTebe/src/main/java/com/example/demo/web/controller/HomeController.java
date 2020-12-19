d900f0015de33c58a7fbca89377201d7087c1191
stojchevska <stojcevska.ivana@gmail.com>
2020-12-19 11:54:15 +0100

    edited home controller

--- домашна 3/AptekaDoTebe/src/main/java/com/example/demo/web/controller/HomeController.java
+++ домашна 3/AptekaDoTebe/src/main/java/com/example/demo/web/controller/HomeController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-public class HomeController {
     @Controller
     @RequestMapping("/home")
     public class HomeController {
@@ -19,4 +18,4 @@ public class HomeController {
             return "home_en";
         }
     }
-}
+
