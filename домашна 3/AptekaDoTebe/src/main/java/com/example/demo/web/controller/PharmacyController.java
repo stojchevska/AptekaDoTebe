e45589bfa9ba9beb65fa888c52f402321d05d09c
stojchevska <stojcevska.ivana@gmail.com>
2020-12-19 11:55:07 +0100

    changes in pharmacy controller

--- домашна 3/AptekaDoTebe/src/main/java/com/example/demo/web/controller/PharmacyController.java
+++ домашна 3/AptekaDoTebe/src/main/java/com/example/demo/web/controller/PharmacyController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-public class PharmacyController {
     @Controller
     @RequestMapping("/pharmacy")
     public class PharmacyController {
@@ -35,4 +34,4 @@ public class PharmacyController {
         }
     }
 
-}
+
