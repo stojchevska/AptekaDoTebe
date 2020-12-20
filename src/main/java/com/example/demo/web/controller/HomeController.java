b0f871124d5145470737aea8519d60fba2de3af3
stojchevska <stojcevska.ivana@gmail.com>
2020-12-20 18:10:01 +0100

    deploy

---
+++ src/main/java/com/example/demo/web/controller/HomeController.java
@@ -0,0 +1,21 @@
+package com.example.demo.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+    @Controller
+    @RequestMapping("/home")
+    public class HomeController {
+
+        @GetMapping("/mk")
+        public String getHomePageMk() {
+            return "home_mk";
+        }
+
+        @GetMapping("/en")
+        public String getHomePageEn() {
+            return "home_en";
+        }
+    }
+
